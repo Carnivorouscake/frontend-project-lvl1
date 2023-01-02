99ff2762c2f18d344ec6964566dc70a69f2d4f71
Carnivorouscake <110086166+Carnivorouscake@users.noreply.github.com>
2023-01-02 13:00:30 +0500

    Update location

--- bin/games/brain-games.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-// eslint-disable-next-line import/no-extraneous-dependencies
-import readlineSync from 'readline-sync';
-
-// eslint-disable-next-line import/extensions
-import sayHello from '../src/cli.js';
-
-sayHello(readlineSync);
